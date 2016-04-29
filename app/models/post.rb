2ef3338fe3bf757018faf5b43aeb445ff0df1829
Sheriff Mogaji <shakmog@gmail.com>
2016-04-29 06:51:08 +0100

    so far so good

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 	has_many :comments
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", show: "500x500>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	has_attached_file :author, styles: { thumb: "50x50>" }, default_url: "/images/:style/missing.png"
